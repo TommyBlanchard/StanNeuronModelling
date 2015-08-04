8e75639e36665dbea483ca85296cea072edcaa07
Steven Piantadosi <spiantado@gmail.com>
2015-08-03 20:42:26 -0400

    added noise to simulated data

--- RegressionModel/run.R
+++ RegressionModel/run.R
@@ -1,31 +1,36 @@
 library(rstan)
 library(MASS) # For mvrnorm
 
+source("../Shared.R")
+
 # make stan parallel 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores()) 
 
-# This command isn't working for me...? options(width=Sys.getenv("COLUMNS"))  # fix the number of columns
 options(width=Sys.getenv("COLUMNS"))  # fix the number of columns
 
 modelcode <- paste(readLines('model.stan'), collapse = '\n')
 
-N_CELLS <- 500
+N_CELLS <- 50
 DATA_PER_CELL <- 100
+
+set.seed(100)
 
 # Make up regression data
 betas <- mvrnorm(n=N_CELLS, mu=c(0,0), Sigma=matrix(c(1,1.5, 1.5,3), nrow=2))
 
-plot(betas)
-abline(h=0,v=0)
+# Show our betas
+# plot(betas)
+# abline(h=0,v=0)
 
 d <- NULL
 for(r in 1:nrow(betas)) {
     b <- betas[r,]
     
-    m <- matrix(runif(DATA_PER_CELL), ncol=2)
+    m <- matrix(runif(DATA_PER_CELL*2), ncol=2)
     
-    d <- rbind(d, data.frame(cell=r, x1=m[,1], x2=m[,2], y=(m %*% b)))
+    err <- rnorm(DATA_PER_CELL, 0.0, 0.10)
+    d <- rbind(d, data.frame(cell=r, x1=m[,1], x2=m[,2], y= m%*%b+err)) 
 }
 
 # If we wanted to recover the coefficients
@@ -36,16 +41,18 @@ data <- list(N_CELLS=N_CELLS,
              M=2, 
              N_RESPONSES=nrow(d),
              dim=2,
-             alpha_cov_mix=c(1,1)*.01, # dirichlet prior
-             alpha_noise_mix=c(1,1),
+             alpha_cov_mix=c(1,1)*.1, # dirichlet prior
+             alpha_noise_mix=c(1,10), # Set a prior favoring signal over noise
              x1=d$x1, x2=d$x2, cell=d$cell, y=d$y
              )            
 
 
 # plot(data$beta) # look at our data
 
-myfit <- stan(model_code=modelcode, data=data, iter=1000, warmup=10, chains=1)
-
+myfit <- stan(model_code=modelcode, data=data, iter=1000, warmup=100, chains=1) #, control=list(stepsize=0.001))
+# myfit <- stan(model_code=modelcode, data=data, iter=10000, warmup=10, chains=1, control=list(adapt_engaged=FALSE), algorithm="HMC")
+# myfit <- stan(model_code=modelcode, data=data, iter=10000, warmup=10, chains=1, algorithm="HMC")
+# myfit <- stan(model_code=modelcode, data=data, iter=500000, warmup=5, chains=1) # Holy crap warmup takes forever
 
 # print(myfit)
 # 
@@ -56,9 +63,22 @@ myfit <- stan(model_code=modelcode, data=data, iter=1000, warmup=10, chains=1)
 # traceplot(myfit, pars = 'residual_variance')
 
 print(myfit, pars='beta')
+
 print(myfit, pars='covs')
+
 print(myfit, pars='mixture_weights')
 
 print(myfit, pars='noise_weight')
 
+print(myfit, pars='scale')
+
+print(myfit, pars='noise_error')
+print(myfit, pars='signal_error')
+
 plot_beta_dif(myfit, betas)
+# plot_covs(myfit, 4)
+
+# get_sampler_params(myfit)
+
+
+summary(do.call(rbind, args = get_sampler_params(myfit, inc_warmup = TRUE)), digits = 2)
