4e8164a86ca66a6edc388d2267c21f772d53c172
TommyBlanchard <blanchard.tomy@gmail.com>
2015-10-05 12:55:12 -0400

    Visualizations for 1 and multi covs updated

--- Shared.R
+++ Shared.R
@@ -1,64 +1,42 @@
 
 # shared R files for plotting etc. 
 library(car)
+library(ellipse)
+library(rstan)
 
-plot_covs <- function(myfit,num_plots){
-  #Plots the betas and draws ellipses to represent the covariance matrices
-  #myfit is the fit given by stan
-  #num_plots is the number of plots you want to see: a value of  2 will
-  #show betas & covariances matrices for halfway through the iterations 
-  #and the last iteration.
-  
-  #TODO: currently will only work with the regression model.
-  # currently plots for just 1 chain, ignores others
-  
+plot_covs <- function(myfit){
   #covs[iteration,chain,param] 
   covs = extract(myfit,pars='covs', permuted='false');
-  beta = extract(myfit,pars='beta', permuted='false');
+  beta = extract(myfit,pars='betas', permuted='false');
   mixture_weights = extract(myfit,pars='mixture_weights', permuted='false');
-  #noise_weight = extract(myfit,pars='noise_weight', permuted='false');
+  noise_weight = extract(myfit,pars='noise_weight', permuted='false');
   scale = extract(myfit, pars='scale', permuted='false');
   
   num_covs = dim(covs)[3]/4;
   num_iter = dim(covs)[1];
   n_cells = dim(beta)[3]/2;
-  
-  iter = floor(num_iter/num_plots);
-  
-  plot_rows = floor(sqrt(num_plots));
-  plot_cols = ceiling(num_plots/plot_rows);
-  par(mfrow=c(plot_rows,plot_cols))
-  for(i in 1:num_plots){
-    print(i)
-    plot_iter = iter*i;
-    plot(beta[plot_iter,1,1:n_cells],beta[plot_iter,1,n_cells+1:n_cells])
-    abline(h=0,v=0)
-    adjusted_weight = mixture_weights[plot_iter,1,];#(1-noise_weight[plot_iter])*
+
+  whichmedian <- function(x) which.min(abs(x - median(x))) 
+    plot_iter = whichmedian(covs[,1,num_covs+1]) #Find the median scale for the first cov matrix - we'll plot this
+    plot(beta[plot_iter,1,1:n_cells],beta[plot_iter,1,n_cells+1:n_cells],xlab="beta for variable 1",ylab="beta for variable 2",cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+    adjusted_weight = mixture_weights[plot_iter,1,]*(1-.5*noise_weight[plot_iter]);
     for(c in 1:num_covs){
-      unscaled_cov = matrix(covs[plot_iter,1,seq(c,length(covs[plot_iter,1,]),by=num_covs)],nrow=2);
-      s = diag(c(scale[plot_iter,1,1], scale[plot_iter,1,2]))
-      cur_cov = s %*% unscaled_cov %*% s
+      plot_scale = scale[plot_iter,1,seq(c,length(scale[plot_iter,1,]), by=num_covs)];
+      corr = matrix(covs[plot_iter,1,seq(c,length(covs[plot_iter,1,]),by=num_covs)],nrow=2);
+      mycol <- col2rgb(palette()[1+c])/255      
       
-      e <- eigen(cur_cov)
-      
-    # From http://stats.stackexchange.com/questions/9898/how-to-plot-an-ellipse-from-eigenvalues-and-eigenvectors-in-r
-    ctr <- c(0,0)
-    angles <- seq(0, 2*pi, length.out=200) 
-      eigVal  <- eigen(cur_cov)$values
-    eigVec  <- eigen(cur_cov)$vectors
-    eigScl  <- eigVec %*% diag(sqrt(eigVal))  # scale eigenvectors to length = square-root
-    xMat    <- rbind(ctr[1] + eigScl[1, ], ctr[1] - eigScl[1, ])
-    yMat    <- rbind(ctr[2] + eigScl[2, ], ctr[2] - eigScl[2, ])
-    ellBase <- cbind(sqrt(eigVal[1])*cos(angles), sqrt(eigVal[2])*sin(angles)) # normal ellipse
-    ellRot  <- eigVec %*% t(ellBase)  # rotated ellipse
-    mycol <- palette()[1+c]
-    lines((ellRot+ctr)[1, ], (ellRot+ctr)[2, ], asp=1, type="l", lwd=2, col=mycol)
-    matlines(xMat, yMat, lty=1, lwd=2, col=mycol)
-    points(ctr[1], ctr[2], pch=4, col=mycol, lwd=3)
-      
-#       ellipse(c(0, 0), shape=cur_cov, radius=1, col=palette()[1+c], fill=TRUE, fill.alpha = adjusted_weight[c])
+      plot_bvnorm_ellipse(corr,plot_scale,adjusted_weight[c],mycol,.25)
+      plot_bvnorm_ellipse(corr,plot_scale,adjusted_weight[c],mycol,.5)
+      plot_bvnorm_ellipse(corr,plot_scale,adjusted_weight[c],mycol,.75)
+      plot_bvnorm_ellipse(corr,plot_scale,adjusted_weight[c],mycol,.95)
+      points(beta[plot_iter,1,1:n_cells],beta[plot_iter,1,n_cells+1:n_cells])
+      abline(0,0,0,0)
     }
-  }
+}
+
+plot_bvnorm_ellipse <-function(corr,scale,weight,col,level){
+  ell = ellipse(corr, scale,level=level)
+  polygon(ell, col=rgb(col[1],col[2],col[3],weight*(1-level)))
 }
 
 plot_beta_dif <- function(myfit,given_betas){
@@ -66,7 +44,7 @@ plot_beta_dif <- function(myfit,given_betas){
   #with the real betas (or supplied betas)
   #Eventually, use this to compare betas to betas obtained through
   #regular linear regression
-  extracted_beta = extract(myfit,pars='beta', permuted='false');
+  extracted_beta = extract(myfit,pars='betas', permuted='false');
   num_iter = dim(extracted_beta)[1];
   n_cells = dim(extracted_beta)[3]/2;
 #   model_beta = matrix(extracted_beta[num_iter,1,],nrow=n_cells);
@@ -103,32 +81,109 @@ hist_cov_angle <- function(myfit,ind1,ind2){
   #print intervals and stuff
 }
 
-hist_eigen_ratio <- function(myfit,ind1){
-  #requires the fit, and the covariance matrix index
-  #plots a histogram of the ratio of the eigenvectors of the cov
-  covs = extract(myfit,pars='covs', permuted='false');
-  scale = extract(myfit, pars='scale', permuted='false');
-  
-  num_covs = dim(covs)[3]/4;
-  num_iter = dim(covs)[1];
-  
-  eig_ratio = numeric(length = num_iter);
-  
-  for(i in 1:num_iter){
-    # Get the eigenvectors for the covariance matrices
-    unscaled_cov = matrix(covs[i,1,seq(ind1,length(covs[i,1,]),by=num_covs)],nrow=2);
-    s = diag(c(scale[i,1,1], scale[i,1,2]));
-    cov1 = s %*% unscaled_cov %*% s;
-    eig1 = eigen(cov1)$values[1];
-    eig2 = eigen(cov1)$values[2];
-    
-    #Calculate ratio - the closer to 1 this is, the more circular. 
-    #The closer to 0, the flatter
-    eig_ratio[i] = eig2/eig1;
+hist_mix_weight <- function(myfit){
+  mix_weight = extract(myfit,pars='mixture_weights', permuted='false');
+  #hist(mix_weight[mix_weight > .9],breaks=seq(from=.9,to=1,by=.005), col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  list_histo=hist(mix_weight,breaks=seq(from=0,to=1,by=.05), col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  x = mix_weight;
+  mx = mean(x);
+  n = length(x);
+  n97 = round(n*.975)
+  n2 = round(n*.025)
+  high_int= sort(x, TRUE)[n97];
+  low_int = sort(x, TRUE)[n2];
+  lines( c(high_int,high_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(high_int, max(list_histo$counts)/2 + 10 , toString(round(high_int, 2)), font=2)
+  lines( c(low_int,low_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(low_int, max(list_histo$counts/2) + 10 , toString(round(low_int, 2)), font=2)
+}
+
+hist_corr <- function(myfit){
+  corr_mat = extract(myfit,pars='covs', permuted='false');
+  correlation = corr_mat[,,2]
+  mx = mean(correlation);
+  n = dim(correlation)[1]*dim(correlation)[2];
+  n97 = round(n*.975)
+  n2 = round(n*.025)
+  high_int= sort(correlation, TRUE)[n97];
+  low_int = sort(correlation, TRUE)[n2];
+  list_histo = hist(correlation,breaks=seq(from=-1,to=1,by=.05), col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  lines( c(mx,mx), c(0,max(list_histo$counts)), col = "red", lwd = 5)
+  text(mx, max(list_histo$counts + 10) , paste("mean =", toString(round(mx, 2))), font=2)
+  lines( c(high_int,high_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(high_int, max(list_histo$counts)/2 + 10 , toString(round(high_int, 2)), font=2)
+  lines( c(low_int,low_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(low_int, max(list_histo$counts/2) + 10 , toString(round(low_int, 2)), font=2)
+}
+
+hist_scale <- function(myfit){
+  scale = extract(myfit,pars='scale', permuted='false');
+  xscale = scale[,,1]
+  hist(xscale, col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  yscale = scale[,,2]
+  hist(yscale, col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+}
+
+hist_scale_ratio <- function(myfit){
+  scale = extract(myfit,pars='scale', permuted='false');
+  xscale = scale[,,1]
+  dim(xscale) <- NULL
+  yscale = scale[,,2]
+  dim(yscale) <- NULL
+  scaledif = xscale - yscale;
+  scale_difference_ratio <- NULL;
+  for(i in 1:length(xscale)){
+    scale_difference_ratio[i] = scaledif[i]/(max(c(xscale[i],yscale[i])))
   }
-  hist(eig_ratio)
-  
-  eig_ratio = eig_ratio[order(eig_ratio)];
-  
-  #print intervals and stuff
+  x = scale_difference_ratio;
+  mx = mean(x);
+  n = length(x);
+  n97 = round(n*.975)
+  n2 = round(n*.025)
+  high_int= sort(x, TRUE)[n97];
+  low_int = sort(x, TRUE)[n2];
+  list_histo = hist(scale_difference_ratio, main = "Histogram of scale ratio", xlab="scale ratio",breaks=seq(from=-1,to=1,by=.05), col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  lines( c(mx,mx), c(0,max(list_histo$counts)), col = "red", lwd = 5)
+  text(mx, max(list_histo$counts + 10) , paste("mean =", toString(round(mx, 2))), font=2)
+  lines( c(high_int,high_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(high_int, max(list_histo$counts)/2 + 10 , toString(round(high_int, 2)), font=2)
+  lines( c(low_int,low_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(low_int, max(list_histo$counts/2) + 10 , toString(round(low_int, 2)), font=2)
+}
+
+hist_noise_weight <- function(myfit){
+  noise_weight = extract(myfit,pars='noise_weight', permuted='false');
+  x = noise_weight;
+  mx = mean(x);
+  n = dim(x)[1]*dim(x)[2];
+  n97 = round(n*.975)
+  n2 = round(n*.025)
+  high_int= sort(x, TRUE)[n97];
+  low_int = sort(x, TRUE)[n2];
+  list_histo = hist(noise_weight,breaks=seq(from=0,to=1,by=.05), col='blue',cex.lab=2, cex.main=2, cex.axis=2, font.lab=2, font.main=2, font.axis=2)
+  lines( c(mx,mx), c(0,max(list_histo$counts)), col = "red", lwd = 5)
+  text(mx, max(list_histo$counts + 10) , paste("mean =", toString(round(mx, 2))), font=2)
+  lines( c(high_int,high_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(high_int, max(list_histo$counts)/2 + 10 , toString(round(high_int, 2)), font=2)
+  lines( c(low_int,low_int), c(0,max(list_histo$counts)/2), col = "green", lwd = 5)
+  text(low_int, max(list_histo$counts/2) + 10 , toString(round(low_int, 2)), font=2)
+}
+
+vis_single_cov <- function(myfit,name){
+  pdf(paste(name, '_figs.pdf',sep=''))
+  hist_corr(myfit)
+  hist_scale(myfit)
+  hist_scale_ratio(myfit)
+  hist_noise_weight(myfit)
+  plot_covs(myfit)
+  dev.off()
+}
+
+vis_multi_cov <- function(myfit,name){
+  pdf(paste(name, '_mix_hist.pdf',sep=''))
+  hist_mix_weight(myfit)
+  dev.off()
+  pdf(paste(name, '_scatter.pdf',sep=''))
+  plot_covs(myfit)
+  dev.off()
 }
